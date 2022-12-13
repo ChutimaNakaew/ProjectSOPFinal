a4edbae673efd072355ae7b0b831779f233fc4f3
ChutimaNakaew <73680207+ChutimaNakaew@users.noreply.github.com>
2022-12-13 18:52:16 +0700

    test

---
+++ src/main/java/com/example/projectsopfinal/Controller/DetailTour.java
@@ -0,0 +1,14 @@
+package com.example.projectsopfinal.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class DetailTour {
+    @RequestMapping(value = "/detail", method = RequestMethod.GET)
+    public String detailTour(){
+        return "DetailTour";
+    }
+}
