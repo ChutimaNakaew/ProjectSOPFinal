d179a373d287fa89ff058d8ac5ce7a24623854a8
ChutimaNakaew <73680207+ChutimaNakaew@users.noreply.github.com>
2022-12-13 16:35:57 +0700

    add

--- src/main/java/com/example/projectsopfinal/ProjectSopFinalApplication.java
+++ src/main/java/com/example/projectsopfinal/ProjectSopFinalApplication.java
@@ -1,13 +1,15 @@
 package com.example.projectsopfinal;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableAutoConfiguration
 @SpringBootApplication
 public class ProjectSopFinalApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProjectSopFinalApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectSopFinalApplication.class, args);
+    }
 
 }
