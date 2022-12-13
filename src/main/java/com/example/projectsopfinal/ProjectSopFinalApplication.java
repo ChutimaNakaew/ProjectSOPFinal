18aea5f077a42f43756bb17528c33f30577e9761
ChutimaNakaew <73680207+ChutimaNakaew@users.noreply.github.com>
2022-12-13 16:30:47 +0700

    Initial commit

---
+++ src/main/java/com/example/projectsopfinal/ProjectSopFinalApplication.java
@@ -0,0 +1,13 @@
+package com.example.projectsopfinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSopFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectSopFinalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/projectsopfinal/ProjectSopFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectsopfinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSopFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
