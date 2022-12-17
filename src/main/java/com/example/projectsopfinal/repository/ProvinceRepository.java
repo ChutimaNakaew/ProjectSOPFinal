d30632f12443071338ac2731c88af956b24a8cdc
thpvrs <63070104@kmitl.ac.th>
2022-12-17 08:47:53 +0700

    comment

--- src/main/java/com/example/projectsopfinal/repository/ProvinceRepository.java
+++ src/main/java/com/example/projectsopfinal/repository/ProvinceRepository.java
@@ -1,13 +1,13 @@
-package com.example.projectsopfinal.repository;
-
-import com.example.projectsopfinal.model.Province;
-import com.example.projectsopfinal.model.Tour;
-import org.springframework.data.mongodb.repository.Query;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProvinceRepository extends CrudRepository<Province, String> {
-    @Query
-    public Province findByName (String name);
-}
+//package com.example.projectsopfinal.repository;
+//
+//import com.example.projectsopfinal.model.Province;
+//import com.example.projectsopfinal.model.Tour;
+//import org.springframework.data.mongodb.repository.Query;
+//import org.springframework.data.repository.CrudRepository;
+//import org.springframework.stereotype.Repository;
+//
+//@Repository
+//public interface ProvinceRepository extends CrudRepository<Province, String> {
+//    @Query
+//    public Province findByName (String name);
+//}
--- src/main/java/com/example/projectsopfinal/repository/ProvinceService.java
+++ src/main/java/com/example/projectsopfinal/repository/ProvinceService.java
@@ -1,33 +1,33 @@
-package com.example.projectsopfinal.repository;
-
-import com.example.projectsopfinal.model.Province;
-import org.springframework.stereotype.Service;
-
-import java.util.Optional;
-
-@Service
-public class ProvinceService {
-    private ProvinceRepository provinceRepository;
-    public ProvinceService(ProvinceRepository provinceRepository){
-        this.provinceRepository = provinceRepository;
-    }
-    public Province saveProvince(Province province){
-        return provinceRepository.save(province);
-    }
-
-    public Iterable<Province> getAllProvinces(){
-        return provinceRepository.findAll();
-    }
-
-    public void deleteAllProvinces(){
-        provinceRepository.deleteAll();
-    }
-
-    public Province detailTourByName(String name){
-        return provinceRepository.findByName(name);
-    }
-
-    public Optional<Province> findTourById(String id){
-        return provinceRepository.findById(id);
-    }
-}
+//package com.example.projectsopfinal.repository;
+//
+//import com.example.projectsopfinal.model.Province;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.Optional;
+//
+//@Service
+//public class ProvinceService {
+//    private ProvinceRepository provinceRepository;
+//    public ProvinceService(ProvinceRepository provinceRepository){
+//        this.provinceRepository = provinceRepository;
+//    }
+//    public Province saveProvince(Province province){
+//        return provinceRepository.save(province);
+//    }
+//
+//    public Iterable<Province> getAllProvinces(){
+//        return provinceRepository.findAll();
+//    }
+//
+//    public void deleteAllProvinces(){
+//        provinceRepository.deleteAll();
+//    }
+//
+//    public Province detailTourByName(String name){
+//        return provinceRepository.findByName(name);
+//    }
+//
+//    public Optional<Province> findTourById(String id){
+//        return provinceRepository.findById(id);
+//    }
+//}
