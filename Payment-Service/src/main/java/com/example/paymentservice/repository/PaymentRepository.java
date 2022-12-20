59767eaa1f613d249c0ece502f120bfc1ccd247a
ChutimaNakaew <73680207+ChutimaNakaew@users.noreply.github.com>
2022-12-20 23:58:00 +0700

    Delete Payment-Service directory

--- Payment-Service/src/main/java/com/example/paymentservice/repository/PaymentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.paymentservice.repository;
-
-import com.example.paymentservice.model.Payment;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PaymentRepository extends CrudRepository<Payment, String> {
-}
